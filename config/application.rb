4dbac5da079c177609310e3d74377f9687c41d4c
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-05-19 08:58:57 +0200

    updated binstubs

--- config/application.rb
+++ config/application.rb
@@ -59,10 +59,12 @@ module Numbers
     # Action Cable setting to de-couple it from the main Rails process.
     config.action_cable.url = ENV['ACTION_CABLE_FRONTEND_URL']
 
-    # Action Cable setting to allow connections from these domains.
-    origins = ENV['ACTION_CABLE_ALLOWED_REQUEST_ORIGINS'].split(',')
-    origins.map! { |url| /#{url}/ }
-    config.action_cable.allowed_request_origins = origins
+    if ENV['ACTION_CABLE_ALLOWED_REQUEST_ORIGINS'].present?
+      # Action Cable setting to allow connections from these domains.
+      origins = ENV['ACTION_CABLE_ALLOWED_REQUEST_ORIGINS'].split(',')
+      origins.map! { |url| /#{url}/ }
+      config.action_cable.allowed_request_origins = origins
+    end
 
     config.generators do |g|
       g.fixture_replacement :fabrication
