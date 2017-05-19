1444b596619ceff8c45a0fda4d79bff54d36d182
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-05-19 09:03:36 +0200

    rails generate rspec:install

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe MembersController, type: :controller do
+  describe MembersController do
+
+    it "/members/total" do
+      bike = Bike.create! valid_attributes
+      get :total
+      expect(assigns(:bikes)).to eq([bike])
+    end
+  end
+
 
 end
