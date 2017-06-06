5f3368561b050c1687b3d498783a0bd4445e5bf5
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-06-06 21:48:53 +0200

    Started on data for sparkline

--- spec/models/member_count_spec.rb
+++ spec/models/member_count_spec.rb
@@ -3,14 +3,21 @@ require 'rails_helper'
 RSpec.describe MemberCount, type: :model do
 
   before :all do
+    MemberCount.destroy_all # Let Database cleaner handle this.
+    Timecop.freeze( DateTime.parse('1.5.2000') )
     MemberCount.create!(kind: :total, count: 10)
+
+    Timecop.freeze( DateTime.parse('2.5.2000') )
     MemberCount.create!(kind: :total, count: 11)
+
+    Timecop.freeze( DateTime.parse('2.5.2000') )
     MemberCount.create!(kind: :total, count: 12)
   end
 
-  it 'returns a timeline' do
+  it 'returns a timeline WIP' do
     puts "=============================\n"
     puts MemberCount.json_for_total
+    puts MemberCount.all.size
     puts "=============================\n"
   end
 end
